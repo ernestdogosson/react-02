a2d31a7e71705a4894b53d416e9e52d110160bc7
Ernest Dogo <dogoernest@outlook.com>
2025-11-26 11:26:55 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="py-8 bg-white text-center">
+        <h1 className="text-4xl font-bold text-gray-700 tracking-widest mb-4">
+          Do you know your flags?
+        </h1>
+        <span className="text-blue-500 ">Test your knowledge</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Api.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Api() {
+  return <div></div>;
+}
+
+export default Api;
